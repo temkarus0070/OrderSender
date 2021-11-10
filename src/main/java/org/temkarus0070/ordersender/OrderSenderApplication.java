1ecf6ded8f50b0d48858a617b2d4eab13dbc4f3f
artyom <pretkovartyom@gmail.com>
2021-11-10 16:37:03 +0300

    repair app logic

--- src/main/java/org/temkarus0070/ordersender/OrderSenderApplication.java
+++ src/main/java/org/temkarus0070/ordersender/OrderSenderApplication.java
@@ -2,8 +2,10 @@ package org.temkarus0070.ordersender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OrderSenderApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/temkarus0070/ordersender/OrderService.java
+++ src/main/java/org/temkarus0070/ordersender/OrderService.java
@@ -7,8 +7,11 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.temkarus0070.models.Order;
 
 import javax.annotation.PostConstruct;
@@ -19,13 +22,17 @@ public class OrderService {
     @Value("${spring.kafka.bootstrap-servers}")
     private String kafkaServer;
 
+    @Value("${order.server}")
+    private String orderGeneratorServer;
+
     @Value("${spring.kafka.template.default-topic}")
     private String topicName;
 
-    private KafkaProducer<Long,Order>kafkaProducer;
-    public OrderService(){
+    @Value("${order.delay}")
+    private int delay;
 
-    }
+    private KafkaProducer<Long,Order>kafkaProducer;
+    public OrderService(){}
 
     @PostConstruct
     public void init(){
@@ -38,9 +45,18 @@ public class OrderService {
         kafkaProducer=new KafkaProducer<>(properties);
     }
 
+    @Scheduled(fixedDelayString = "${order.delay}")
+    public void getOrder(){
+        RestTemplate restTemplate=new RestTemplate();
+        ResponseEntity<Order> order=restTemplate.getForEntity(orderGeneratorServer+"/generateOrder",Order.class);
+        sendToQueue(order.getBody());
+    }
+
 
     public void sendToQueue(Order order){
         ProducerRecord<Long,Order> producerRecord=new ProducerRecord<>(topicName,order.getOrderNum(),order);
-        kafkaProducer.send(producerRecord);
+        if(kafkaProducer.send(producerRecord).isDone()){
+            System.out.println("done");
+        }
     }
 }
